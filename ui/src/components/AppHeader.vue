bb93a150484d383551436b50c3681ce67bbc9a42
Lauri Lahti <1960498+msha@users.noreply.github.com>
2026-04-29 04:20:41 +0300

    stufs

--- ui/src/components/AppHeader.vue
+++ ui/src/components/AppHeader.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-defineProps<{ runName: string; statusText: string }>()
+defineProps<{ runName: string; statusText: string }>();
 </script>
 
 <template>
-  <header>
-    <span class="brand" title="JEPA-RL: Joint-Embedding Predictive Architecture for Reinforcement Learning">jepa-rl</span>
-    <span class="sep"></span>
-    <span class="run-name" title="Current active run directory">{{ runName }}</span>
-    <span class="header-status">{{ statusText }}</span>
-  </header>
+    <header>
+        <span
+            class="brand"
+            title="JEPA-RL: Joint-Embedding Predictive Architecture for Reinforcement Learning"
+            >AGI @ Home</span
+        >
+        <span class="sep"></span>
+        <span class="run-name" title="Current active run directory">{{
+            runName
+        }}</span>
+        <span class="header-status">{{ statusText }}</span>
+    </header>
 </template>
